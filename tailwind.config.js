1d697a8d456521c59b63b85e86bdd2f2289bb526
colaturka <130190241+colaturka@users.noreply.github.com>
2024-03-09 23:41:49 -0600

    new react app

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './myapp/src/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
